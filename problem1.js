a945fcc4e91a10581a3b859714c791cfcf287cc8
EnrickaM <marshallenricka@yahoo.com>
2019-01-16 12:03:39 -0600

    classwork

---
+++ problem1.js
@@ -0,0 +1,26 @@
+//Put the start of your program in a main function.
+// Ask the user to enter a number to print.
+
+
+
+main();
+    function main() {
+        var number = prompt("Enter a number");
+        switch (number) {
+            case "1" :
+                console.log(1);
+                break;
+            case "2" :
+                console.log(2);
+                break;
+            case "3" :
+                console.log(3);
+                break;
+            case "4" :
+                console.log(4);
+                break;
+            case "5" :
+                console.log(5);
+
+        }
+    }
---
+++ problem3.js
@@ -0,0 +1,15 @@
+// Put the start of your program in a main function.
+// Create an attendance array.
+// Ask the user if a teacher, student, or parent is checking in.
+// Use a switch statement to add the teacher/student/parent to the attendance array, then log the option they chose.
+// Repeat this five times.
+
+
+
+
+main();
+    function main() {
+        var attendancearray = ["teacher", "student", "parent"];
+        var attendance = prompt("Who is checking in?");
+
+    }
